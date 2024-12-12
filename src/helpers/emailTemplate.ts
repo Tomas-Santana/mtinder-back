d406984dd1dde8094d4cb33f79afee20f30b818f
MarlonUrda <MGUR2025pro@gmail.com>
2024-12-12 04:57:29 -0400

    correo listo

--- src/helpers/emailTemplate.ts
+++ src/helpers/emailTemplate.ts
@@ -8,27 +8,27 @@ export const emailTemplate = (user: string, code: string) => {
     <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
     <meta name="x-apple-disable-message-reformatting" /><!--$-->
   </head>
-  <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Cambia tu contraseña de Rotten Minds
+  <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Cambia tu contraseña de Mellow Mates
   </div>
 
-  <body style="background-color:transparent;padding:10px 0">
-    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:37.5em;background-color:transparent;border:3px solid #000000;padding:45px">
+  <body style="background-color:#191919;padding:10px 0">
+    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:37.5em;background-color:#191919;border:3px solid #ffffff;padding:45px">
       <tbody>
         <tr style="width:100%">
           <td>
             <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
               <tbody>
                 <tr>
-                  <td><img alt="Rotten Minds" height="100" src="https://firebasestorage.googleapis.com/v0/b/cervant-admin-panel.appspot.com/o/tenants%2Fpublic%2Fnotebit%2Flogo2.png?alt=media" style="display:block;outline:none;border:none;text-decoration:none;" widht="750"/>
-                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:black">Hola ${user},</p>
-                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:black">
-					Alguien solicitó un cambio de contraseña para tu cuenta de Rotten Minds. Si fuiste tú, usa el siguiente código para cambiar tu contraseña:</p>
+                  <td><img alt="Mellow Mates" height="100" src="https://firebasestorage.googleapis.com/v0/b/mellow-mates-db93c.firebasestorage.app/o/public%2Femail%2Fmmlogo.png?alt=media" style="display:block;outline:none;border:none;text-decoration:none;" widht="750"/>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:white">Hola ${user},</p>
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:white">
+					Alguien solicitó un cambio de contraseña para tu cuenta de Mellow Mates. Si fuiste tú, usa el siguiente código para cambiar tu contraseña:</p>
 					
 
-					<h2 style="font-size:24px;line-height:32px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:600;color:black">${code}</h2>
+					<h2 style="font-size:24px;line-height:32px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:600;color:white">${code}</h2>
 
 
-                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:black">
+                    <p style="font-size:16px;line-height:26px;margin:16px 0;font-family:&#x27;Open Sans&#x27;, &#x27;HelveticaNeue-Light&#x27;, &#x27;Helvetica Neue Light&#x27;, &#x27;Helvetica Neue&#x27;, Helvetica, Arial, &#x27;Lucida Grande&#x27;, sans-serif;font-weight:300;color:white">
 					Si no solicitaste un cambio de contraseña, ignora este mensaje.
 					</p>
                   </td>
